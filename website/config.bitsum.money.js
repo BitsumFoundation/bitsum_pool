897647d9a378a31b5f9a09e6843085b0e6ed7db7
Dmitry <dmitry.tarasov@live.com>
2018-03-02 00:58:44 +0500

    Bitsum adaptations

--- website/config.bitsum.money.js
+++ website/config.bitsum.money.js
@@ -1,8 +1,8 @@
-var api = "http://pool.bitsum.uz:8117";
+var api = "http://pool.bitsum.money:8117";
 
-var api_blockexplorer = "http://pool.bitsum.uz:18081";
+var api_blockexplorer = "http://pool.bitsum.money:18081";
 
-var poolHost = "pool.bitsum.uz";
+var poolHost = "pool.bitsum.money";
 
 var irc = "";
 
@@ -21,6 +21,7 @@ var themeCss = "themes/default-theme.css";
 var networkStat = {
     "sum": [
         ["pool.bitsum.uz", "http://pool.bitsum.uz:8117"],
+		["pool.bitsum.money", "http://pool.bitsum.money:8117"],
         ["sum.dreampool.info", "https://sum.dreampool.info/api/"]
     ]
 };
