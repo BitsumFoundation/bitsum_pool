b0f22c224c45480ee4e97e47e4c94e2161710238
Dmitry <dmitry.tarasov@live.com>
2018-03-05 20:53:39 +0500

    +New pools *README.md

--- website/config.js
+++ website/config.js
@@ -21,6 +21,9 @@ var themeCss = "themes/default-theme.css";
 var networkStat = {
     "sum": [
         ["pool.bitsum.uz", "http://pool.bitsum.uz:8117"],
-        ["sum.dreampool.info", "https://sum.dreampool.info/api/"]
+        ["sum.dreampool.info", "https://sum.dreampool.info/api/"],
+		["sum.multipool.online", "http://sum.multipool.online:8120"],
+		["sum.gonspool.com", "http://sum.gonspool.com:8118"],
+		["npool.pw/bitsum", "http://sum.npool.pw:10070"],
     ]
 };
