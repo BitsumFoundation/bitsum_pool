5aa6fa21d2e15d209b18eed460c7bbc52a815111
Dmitry Tarasov <dmitry.tarasov@live.com>
2018-06-24 01:59:53 +0500

    +CN Lite V1 support +New payment API

--- website/config.js
+++ website/config.js
@@ -1,6 +1,6 @@
 var api = "http://pool.bitsum.uz:8080";
 
-var api_blockexplorer = "http://explorer.bitsum.uz:18081";
+var api_blockexplorer = "http://explorer.bitsum.uz:28081";
 
 var poolHost = "pool.bitsum.uz";
 
@@ -21,11 +21,13 @@ var themeCss = "themes/default-theme.css";
 var networkStat = {
     "bsm": [
 		["pool.bitsum.uz", "http://pool.bitsum.uz:8080"],
-		["sum.eth.uz [TAS-IX]", "http://sum.eth.uz:8117"],
+		["uz.pool.bitsum.uz (Uzbekistan)", "http://uz.pool.bitsum.uz:8080"],
+		["vn.pool.bitsum.uz (Vietnam)", "http://vn.pool.bitsum.uz:8080"],
+		["bsm.multipool.online", "http://bsm.multipool.online:8120"],
 		["sum.dreampool.info", "https://sum.dreampool.info/api/"],
-		["sum.multipool.online", "http://sum.multipool.online:8120"],
 		["sum.gonspool.com", "http://sum.gonspool.com:8118"],
 		["npool.pw/bitsum", "http://sum.npool.pw:10070"],
 		["pool-best.hopto.org", "http://pool-best.hopto.org:8117"],
+		["youpool.io/BSM", "http://47.98.251.202:8110"],
     ]
 };
